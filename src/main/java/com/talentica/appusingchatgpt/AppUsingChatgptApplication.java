a61dde9ff1c885004e73205aa410b19f2e353c67
ankitwasnik <ankitwasnik7@gmail.com>
2023-04-26 13:47:28 +0530

    Init

---
+++ src/main/java/com/talentica/appusingchatgpt/AppUsingChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.talentica.appusingchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUsingChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUsingChatgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentica/appusingchatgpt/AppUsingChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentica.appusingchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUsingChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
